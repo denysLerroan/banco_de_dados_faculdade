c88a10a8c73a23d4d03152ea30f5e41e9d8612a7
denysLerroan <lerroandeco@gmail.com>
2020-08-17 20:33:46 -0300

    SQL - Creation of the DB and Tables - GT

---
+++ sql_guia_turistico_a1.sql
@@ -0,0 +1,38 @@
+use gt;
+
+CREATE TABLE IF NOT EXISTS pais (
+	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL DEFAULT '',
+    continente ENUM ('Ásia', 'Europa', 'América', 'África', 'Oceania', 'Antártida')
+    NOT NULL DEFAULT 'América',
+    codigo CHAR(3) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE IF NOT EXISTS estado (
+	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL DEFAULT '',
+    sigla CHAR(2) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE IF NOT EXISTS cidade (
+	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL DEFAULT '',
+    população int(11) NOT NULL DEFAULT '0'
+);
+
+CREATE TABLE IF NOT EXISTS ponto_tur (
+	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL DEFAULT '',
+    população int(11) NOT NULL DEFAULT '0',
+    tipo ENUM ('Atrativo', 'Serviço', 'Equipamento', 'Infraestrutura', 'Instituição', 'Organização'),
+    publicado ENUM ('Sim', 'Não') NOT NULL DEFAULT 'Não'
+);
+
+CREATE TABLE coordenada (
+	latitude FLOAT(10,6),
+    longitude FLOAT(10,6)
+);
+
+SHOW TABLES;
+
+SHOW COLUMNS FROM cidade;
